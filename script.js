71b171bd9f4ef6d2113bc0de1bfef97c35c29431
Georgi Datskov <g.datskov@gmail.com>
2025-08-19 11:13:13 +0300

    Add meta data, helpers, adjust styles

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const $ = s => document.querySelector(s);
+const box = $('#box');
+const input = $('#color-input');
+
 document.getElementById('apply-btn').addEventListener('click', function () {
   var c = document.getElementById('color-input').value;
   document.getElementById('box').style.backgroundColor = c;
